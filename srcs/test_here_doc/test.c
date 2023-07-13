2c4f769c6118c34b6bcc724bc73e5e060bae3085
Marine <marine@MacBook-Air-de-Marine-2.local>
2023-07-13 16:21:49 +0200

    prework for bonus done, need to integrate into code

--- srcs/test_here_doc/test.c
+++ srcs/test_here_doc/test.c
@@ -15,17 +15,20 @@ int main(int argc, char **argv)
 	if (strcmp(argv[1], "here_doc") != 0)
 		return(0);
 	path = ".goinfre/here_doc";
-	int res = open(path, O_WRONLY | O_CREAT | O_APPEND, 0644);
-	printf("res : %d\n", res);
-	if (res < 0)
+	int fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	printf("fd : %d\n", fd);
+	if (fd < 0)
 		return (-1);
 		
 	/*test gnl*/
 	write(1,"hererdoc>", 10);
 	result = ft_get_next_line(0);
-	while (result && (ft_strlen(delimiter) != ft_strlen(result) || ft_strncmp(delimiter, result, strlen(delimiter)) != 0))
+	while (result)
 	{
-		printf("%s", result);
+		if ((strlen(result) == (ft_strlen(delimiter) + 1)) && ft_strncmp(delimiter, result, (strlen(delimiter))) == 0)
+		 	break;
+		//printf("%s", result);
+		write(fd, result, strlen(result));
 		free(result);
 		write(1,"hererdoc>", 10);
 		result = ft_get_next_line(0);
