6137e49f6e41b82055344a1912e275312bb149fb
Marine DAVID <madavid@paul-f4Ar4s5.clusters.42paris.fr>
2023-07-25 19:13:57 +0200

    new printf works with signed int now

--- libft/ft_printf_fd _new.c
+++ libft/ft_printf_fd _new.c
@@ -6,16 +6,19 @@
 /*   By: madavid <madavid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 03:11:21 by marine            #+#    #+#             */
-/*   Updated: 2023/07/19 15:02:35 by madavid          ###   ########.fr       */
+/*   Updated: 2023/07/25 19:13:14 by madavid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*attention a verifier si meme nb dargs que de va*/
 
 # include <stdio.h>
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <stddef.h>
+# include "libft.h"
+
 
 void	ft_putstr_pf(int output, char *str, int *n)
 {
@@ -53,6 +56,28 @@ void	ft_addstr(char *buffer, char *str, int *n)
 			i++;
 		}
 	}
+}
+int	ft_addnbr_pf(char *buffer, long long int nb, int *n, int is_unsigned)
+{
+	int		i;
+	char *number;
+
+	if (!is_unsigned && *n < 0)
+	{
+		buffer[*n++] = '-';
+		nb *= -1;
+	}
+	i = 0;
+	number = ft_itoa(nb);
+	if (number == NULL)
+		return (-1);
+	while (number[i])
+	{
+		buffer[*n] = number[i++];
+		*n += 1;
+	}
+	free(number);
+	return (0);
 }
 
 // void	ft_putnbr_pf(char *buffer, long long int nb, int *n, int is_unsigned)
@@ -101,14 +126,14 @@ void	ft_addstr(char *buffer, char *str, int *n)
 // 	ft_puthexa(buffer, ptr, n, 0);
 // }
 
-void	ft_args_pf(char c, va_list ap, int *n, char *buffer)
+void	ft_args(char c, va_list ap, int *n, char *buffer)
 {
 	if (c == 's')
 		ft_addstr(buffer, va_arg(ap, char *), n);
 	// else if (c == 'p')
 	// 	ft_putptr_pf(buffer, va_arg(ap, unsigned long int), n);
-	// else if (c == 'd' || c == 'i')
-	// 	ft_putnbr_pf(buffer, va_arg(ap, int), n, 0);
+	else if (c == 'd' || c == 'i')
+		ft_addnbr_pf(buffer, va_arg(ap, int), n, 0);
 	// else if (c == 'u')
 	// 	ft_putnbr_pf(buffer, va_arg(ap, unsigned int), n, 1);
 	// else if (c == 'x')
@@ -182,7 +207,10 @@ int	ft_countnbr(long long int nb,int is_unsigned)
 void	ft_count_arg_len(char c, va_list ap, int *n)
 {
 	if (c == 'c')
+	{
 		*n += 1;
+		va_arg(ap, char *);
+	}
 	else if (c == 's' )
 		*n = *n + ft_countstr(va_arg(ap, char *));
 	else if (c == 'p')
@@ -207,27 +235,27 @@ int	ft_count(const char *str, va_list ap)
 	{
 		if (str[i] != '%')
 		{
-			printf("str[i] : %c dans boucle normale\n", str[i]);
+			//printf("str[i] : %c dans boucle normale\n", str[i]);
 			i++;
 			n++;
 		}
 		else
 		{
-			printf("str[i] : %c dans boucle speciale\n", str[i]);
+			//printf("str[i] : %c dans boucle speciale\n", str[i]);
 			if (str[i] == '%' && str[i + 1] == '%')
 			{	
-				printf("boucle %%\n");
+				//printf("boucle %%\n");
 				i += 2;
 				n++;
 			}
 			else
 			{
-				printf("boucle comptage\n");	
+				//printf("boucle comptage\n");	
 				ft_count_arg_len(str[i + 1], ap, &n);
 			}
 			i += 2;
 		}
-		printf("n : %d\n", n);
+		//printf("n : %d\n", n);
 	}
 	return (n);
 }
@@ -280,7 +308,7 @@ void	fill_buffer(char *buffer, int *n, const char *str, va_list ap)
 					*n += 1;
 				}
 				else
-					ft_args_pf(str[i + 1], ap, n, buffer);
+					ft_args(str[i + 1], ap, n, buffer);
 			}
 			i += 2;
 		}
@@ -309,21 +337,22 @@ int	ft_printf(int output, const char *str, ...)
 	if (buffer == NULL)
 		return (-1);
 	va_start(ap, str);
-	//fill_buffer(buffer, &n, str, ap);
+	fill_buffer(buffer, &n, str, ap);
 	va_end(ap);
 	n = 0;
-	//ft_putstr_pf(output, buffer, &n);
+	ft_putstr_pf(output, buffer, &n);
+	free(buffer);
 	return (n);
 }
 
 int	main(void)
 {
+	// int b = 12;
 	// /* test ecriture*/
 
 	// ft_printf(1, "Coucou\n");
 
 	/*test longueur*/
-	//int b = 12;
 	// printf("test normal\n");
 	// printf("ft_printf\t%d\n", ft_printf(1, "Coucou\n"));
 	// printf("printf\t\t%d\n", printf("Coucou\n"));
@@ -333,26 +362,26 @@ int	main(void)
 	// printf("ft_printf\t%d\n", ft_printf(1, "|Coucou %c comment ça va %c hihi %c|\n", 'd', 'r','e'));
 	// printf("printf\t\t%d\n", printf("|Coucou %c comment ça va %c hihi %c|\n", 'd', 'r', 'e'));
 	// printf("\n");
-	//char *str = NULL;
 
 
+	// char *str = NULL;
 	// printf("test %%s\n");
 	// printf("ft_printf : |%d|", ft_printf(1, "Coucou %s hihi %s grrr %s\n", str, "bieng ou bieng", "grrr ntm"));
 	// printf("printf : |%d|", printf("Coucou %s hihi %s grrr %s\n", str, "bieng ou bieng", "grrr ntm"));
-	// //printf("printf\t%d\n", printf("Coucou %s \n", "wesh alors"));
-	// printf("\n");
-	
-	printf("test multiple\n");
-	printf("ft_printf : |%d|", ft_printf(1, "Coucou %s %c hihi %c grrr \n", "coucou", 'x', 't'));
-	printf("printf : |%d|", printf("Coucou %s %c hihi %c grrr \n", "coucou", 'x', 't'));
 	//printf("printf\t%d\n", printf("Coucou %s \n", "wesh alors"));
-	printf("\n");
+	// printf("\n");
 
 	// printf("test %%d\n");
-	// printf("ft_printf\t%d\n", ft_printf(1, "Coucou %d \n", 1024));
-	// printf("printf\t%d\n", printf("Coucou %d \n", 1024));
+	// printf("ft_printf\t%d\n", ft_printf(1, "Coucou |%d| |%d| |%d|\n", 1024, 0, -1));
+	// printf("printf\t%d\n", printf("Coucou |%d| |%d| |%d|\n", 1024, 0, -1));
 	// printf("\n");
 	
+/* ici */
+	
+	printf("test multiple\n");
+	printf("ft_printf : |%d|", ft_printf(1, "Coucou |%s| |%c| hihi |%c| |%s| |%d| grrr |%d|\n", "coucou",  'x', 't', "ntm", -605874, -7));
+	printf("printf : |%d|", printf("Coucou |%s| |%c| hihi |%c| |%s| |%d| grrr |%d|\n", "coucou",  'x', 't', "ntm", -605874, -7));
+	printf("\n");
 
 	// printf("test %%i\n");
 	// printf("ft_printf\t%d\n", ft_printf(1, "Coucou %i \n", 1024));
